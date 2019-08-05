b1533e75727dc70e9b1d900d5dc7a9b5cef21f4b
anmavtb <vincent.guiraudou@gmail.com>
2019-08-05 17:57:44 +0200

    simplification de l'exemple

--- exempleClicker/clicker.js
+++ exempleClicker/clicker.js
@@ -1,49 +1,49 @@
-var score = 2000;
+var score = 0;
 var multiplicateur = 0;
 var nbr1 = 0, nbr2 = 0;
 var price1 = 20, price2 = 200;
 
 
 
-document.getElementById('affichage').innerHTML = score.toFixed(0) + "P";
-document.title = score.toFixed(0) + "P - Poké Clicker";
+document.getElementById('affichage').innerHTML = score + "€";
+document.title = score + "€ - Exemple";
 
 
 
-function clickBall() {
+function clickBouton() {
     score += 1 + multiplicateur;
-    document.getElementById('affichage').innerHTML = score.toFixed(0) + "P";
-    document.title = score.toFixed(0) + "P - Poké Clicker";
+    document.getElementById('affichage').innerHTML = score + "€";
+    document.title = score + "€ - Exemple";
 }
 
-function getScout() {
+function getMulti() {
     if (score >= price1) {
         nbr1++;
         multiplicateur++;
         score -= price1;
-        price1 *= 1.1;
-        document.getElementById('affichage').innerHTML = score.toFixed(0) + "P";
-        document.title = score.toFixed(0) + "P - Poké Clicker";
-        document.getElementById('price1').innerHTML = price1.toFixed(0);
+        price1 *= 2;
+        document.getElementById('affichage').innerHTML = score + "€";
+        document.title = score + "€ - Exemple";
+        document.getElementById('price1').innerHTML = price1;
         document.getElementById('nbr1').innerHTML = nbr1;
     }
 }
 
-function getPond(){
+function getAutoClicker(){
     if (score >= price2) {
         nbr2++;
         score -= price2;
-        price2 *= 1.5;
-        document.getElementById('affichage').innerHTML = score.toFixed(0) + "P";
-        document.title = score.toFixed(0) + "P - Poké Clicker";
-        document.getElementById('price2').innerHTML = price2.toFixed(0);
+        price2 *= 2;
+        document.getElementById('affichage').innerHTML = score + "€";
+        document.title = score + "€ - Exemple";
+        document.getElementById('price2').innerHTML = price2;
         document.getElementById('nbr2').innerHTML = nbr2;
-        setInterval(pond,1000);
+        setInterval(autoClicker,1000);
     }
 }
 
-function pond() {
+function autoClicker() {
     score++;
-    document.getElementById('affichage').innerHTML = score.toFixed(0) + "P";
-    document.title = score.toFixed(0) + "P - Poké Clicker";
+    document.getElementById('affichage').innerHTML = score + "€";
+    document.title = score + "€ - Exemple";
 }
